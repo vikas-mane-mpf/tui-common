411ce6dc15c2b672d1c30487eb1a89714769dc3d
vikas-mane-mpf <43613886+vikas-mane-mpf@users.noreply.github.com>
2019-04-17 18:50:35 +0530

    Update ConfigService.js

--- src/services/ConfigService.js
+++ src/services/ConfigService.js
@@ -1,4 +1,4 @@
-define("services/ConfigService", ['underscore', 'jquery', 'backbone', 'main/services/CacheService', 'main/services/RemoteService'], 
+define("ConfigService", ['underscore', 'jquery', 'backbone', 'main/services/CacheService', 'main/services/RemoteService'], 
 function(_, $, Backbone, CacheService, RemoteService){
 
 	var ConfigService = function(){}
