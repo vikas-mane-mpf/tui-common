13f25fc053cd068c1bd26f13d2d867d6d45ffa6e
Vikas Mane <Vikas.mane@Synechron-RM.Local>
2019-04-22 18:30:32 +0530

    reoredering

--- src/main.js
+++ src/main.js
@@ -1,19 +1,6 @@
 define(function (require) {
     'use strict';
-    var AccountService = require("services/AccountService");
-    var CacheService = require("services/CacheService");
-    var ConfigService = require("services/ConfigService");
-    var ErrorService = require("services/ErrorService");
-    var FeatureToggleService = require("services/FeatureToggleService");
-    var FileDownloadService = require("services/FileDownloadService");
-    var FrameService = require("services/FrameService");
-    var HierarchicalService = require("services/HierarchicalService");
-    var MenuService = require("services/MenuService");
-    var RefDataService = require("services/RefDataService");
-    var SecurityService = require("services/SecurityService");
-    var TagRService = require("services/TagRService");
-    var UserService = require("services/UserService");
-
+    
     var behaviorChart = require("components/behaviorChart/behaviorChart");
     var RelevancyChartView = require("components/bubble/bubbleChart");
     var ComboFilter = require("components/comboFilter/ComboFilter");
@@ -42,6 +29,21 @@ define(function (require) {
     var SubscriptionToggle = require("components/statusDropdown/subscriptionToggle");
     var StatusIcon = require("components/statusIcon/StatusIcon");
     var URLVars = require("components/URLVars/URLVars");
+
+    var CacheService = require("services/CacheService");
+    var ConfigService = require("services/ConfigService");
+    var ErrorService = require("services/ErrorService");
+    var RefDataService = require("services/RefDataService");
+    var RemoteService = require("services/RemoteService");
+    var AccountService = require("services/AccountService");
+    var SecurityService = require("services/SecurityService");
+    var FeatureToggleService = require("services/FeatureToggleService");
+    var FileDownloadService = require("services/FileDownloadService");
+    var FrameService = require("services/FrameService");
+    var HierarchicalService = require("services/HierarchicalService");
+    var MenuService = require("services/MenuService");
+    var TagRService = require("services/TagRService");
+    var UserService = require("services/UserService");
 
     var Cookie = require("utils/Cookie");
     var Extensions = require("utils/Extensions");
@@ -137,6 +139,7 @@ define(function (require) {
             RefDataService: RefDataService,
             SecurityService: SecurityService,
             TagRService: TagRService,
+            RemoteService: RemoteService,
             UserService: UserService
         },
         utils: {
