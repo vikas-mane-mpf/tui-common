1dfd129ea1a8b77382a5420ea1b5f249ad157054
Vikas Mane <Vikas.mane@Synechron-RM.Local>
2019-04-23 13:30:32 +0530

    menu json path issue

--- src/models/MenuConfigModel.js
+++ src/models/MenuConfigModel.js
@@ -12,7 +12,7 @@ define(
 		 	var menuCollection = this.canShowNewMenu && this.multilevelMenu ? 'menu.json' : 'menu-old.json';
 			var MenuConfigModel = Backbone.Model.extend({
 
-				url : "static/js/data/"+ menuCollection +"?version="+CacheService.get("currentVersion"),
+				url : "data/"+ menuCollection +"?version="+CacheService.get("currentVersion"),
 
 				initialize : function(options){
 					options = options || {};
